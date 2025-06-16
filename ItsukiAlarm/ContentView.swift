3872a234f8e3a726041c3c0213c0e86da45e8c3a
itsuki <itsuki.enjyoy@gmail.com>
2025-06-16 10:00:55 +0900

    Initial Commit

---
+++ ItsukiAlarm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ItsukiAlarm
+//
+//  Created by Itsuki on 2025/06/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ItsukiAlarm/ItsukiAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItsukiAlarmApp.swift
+//  ItsukiAlarm
+//
+//  Created by Itsuki on 2025/06/16.
+//
+
+import SwiftUI
+
+@main
+struct ItsukiAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
